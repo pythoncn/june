c76cf23d5aaead72f63dc712db9f4057f75eb55c
lepture <lepture@me.com>
2012-03-05 00:17:17 +0800

    fix on link pattern

--- june/filters.py
+++ june/filters.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import re
 import markdown
 from tornado import escape
@@ -10,9 +11,28 @@ def safe_markdown(text):
     text = escape.xhtml_escape(text)
 
     # get link back
-    pattern = (r'(?:&lt;)?(https?://([^/:]+\.[a-z]{2,10}|([0-9]{1,3}\.){3}'
-               '[0-9]{1,3})(:[0-9]+)?(\/.*)?)(?:&gt;)?')
-    text = re.sub(pattern, r'<a href="\1" rel="nofollow">\1</a>', text)
+    def make_link(m):
+        link = m.group(1)
+        title = link.replace('http://', '').replace('https://', '')
+        if len(title) > 30:
+            title = title[:20] + '...'
+        if link.startswith('http://') or link.startswith('https://'):
+            return '<a href="%s" rel="nofollow">%s</a>' % (link, title)
+        return '<a href="http://%s" rel="nofollow">%s</a>' % (link, title)
+
+    # http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+    pattern = re.compile(
+        r'(?m)^((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}'
+        r'/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+'
+        r'|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))')
+    text = pattern.sub(make_link, text)
+
+    pattern = re.compile(
+        r'(?i)(?:&lt;)((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}'
+        r'/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+'
+        r'|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))(?:&gt;)')
+
+    text = pattern.sub(make_link, text)
 
     pattern = re.compile(r'```(\w+)(.+?)```', re.S)
     formatter = HtmlFormatter(noclasses=False)
