5709e15e680dcecb6ed03efa0c1aa52626fe71ce
lepture <lepture@me.com>
2012-05-12 11:57:19 +0800

    topic list style

--- june/topic/lib.py
+++ june/topic/lib.py
@@ -3,12 +3,22 @@ from june.account.models import Member
 from june.node.models import Node
 
 
+def get_user_id_list(topics):
+    for t in topics:
+        yield t.user_id
+        if t.last_reply_by:
+            yield t.last_reply_by
+
+
 def get_full_topics(topics):
-    users = get_cache_list(Member, (t.user_id for t in topics), 'member:')
+    users = get_cache_list(Member, get_user_id_list(topics), 'member:')
     nodes = get_cache_list(Node, (t.node_id for t in topics), 'node:')
-    print users
     for topic in topics:
         if topic.user_id in users and topic.node_id in nodes:
             topic.user = users[topic.user_id]
+            if topic.last_reply_by:
+                topic.replyer = users[topic.user_id]
+            else:
+                topic.replyer = None
             topic.node = nodes[topic.node_id]
             yield topic
