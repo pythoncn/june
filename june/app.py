18ce12d409e72000ff40073efde472e4ef96b351
lepture <lepture@me.com>
2012-05-18 21:28:20 +0800

    style changed for app.py

--- june/app.py
+++ june/app.py
@@ -14,60 +14,50 @@ from tornado.options import define, options
 from july.app import JulyApplication
 from july.web import run_server
 
-_first_run = True
-if _first_run:
-    define('debug', True, type=bool)
-    define('autoescape', None)
+define('debug', True, type=bool)
+define('autoescape', None)
 
-    # site config
-    define('sitename', 'June', type=str)
-    define('version', '1.1.0', type=str)
-    define('siteurl', 'http://lepture.com/project/june/')
-    define('sitefeed', '/feed')
+# site config
+define('sitename', 'June', type=str)
+define('version', '1.1.0', type=str)
+define('siteurl', 'http://lepture.com/project/june/')
+define('sitefeed', '/feed')
 
-    define('static_path', os.path.join(PROJDIR, '_static'))
-    define('static_url_prefix', '/static/', type=str)
-    define('template_path', os.path.join(PROJDIR, "_templates"))
+define('static_path', os.path.join(PROJDIR, '_static'))
+define('static_url_prefix', '/static/', type=str)
+define('template_path', os.path.join(PROJDIR, "_templates"))
 
-    options.locale_path = os.path.join(PROJDIR, '_locale')
-    define('login_url', '/account/signin', type=str)
+options.locale_path = os.path.join(PROJDIR, '_locale')
+define('login_url', '/account/signin', type=str)
 
-    # factor config
-    define('reply_factor_for_topic', 600, type=int)
-    define('reply_time_factor', 1000, type=int)
-    define('up_factor_for_topic', 1500, type=int)
-    define('up_factor_for_user', 1, type=int)
-    define('down_factor_for_topic', 800, type=int)
-    define('down_factor_for_user', 1, type=int)
-    define('accept_reply_factor_for_user', 1, type=int)
-    define('up_max_for_user', 10, type=int)
-    define('down_max_for_user', 4, type=int)
-    define('vote_max_for_user', 4, type=int)
-    define('promote_topic_cost', 100, type=int)
+# factor config
+define('reply_factor_for_topic', 600, type=int)
+define('reply_time_factor', 1000, type=int)
+define('up_factor_for_topic', 1500, type=int)
+define('up_factor_for_user', 1, type=int)
+define('down_factor_for_topic', 800, type=int)
+define('down_factor_for_user', 1, type=int)
+define('accept_reply_factor_for_user', 1, type=int)
+define('up_max_for_user', 10, type=int)
+define('down_max_for_user', 4, type=int)
+define('vote_max_for_user', 4, type=int)
+define('promote_topic_cost', 100, type=int)
 
-    # third party support config
-    define('gravatar_base_url', "http://www.gravatar.com/avatar/")
-    define('gravatar_extra', '')
-    define('recaptcha_key', '')
-    define('recaptcha_secret', '')
-    define('recaptcha_theme', 'clean')
-    define('emoji_url', '')
-    define('ga', '')  # google analytics
-    define('gcse', '')  # google custom search
+# third party support config
+define('gravatar_base_url', "http://www.gravatar.com/avatar/")
+define('gravatar_extra', '')
+define('recaptcha_key', '')
+define('recaptcha_secret', '')
+define('recaptcha_theme', 'clean')
+define('emoji_url', '')
+define('ga', '')  # google analytics
+define('gcse', '')  # google custom search
 
-    # image backend
-    define('backend', 'june.backend.local.LocalBackend')
-
-    _first_run = False
+# image backend
+define('backend', 'june.backend.local.LocalBackend')
 
 
-def main():
-    from july.util import parse_config_file
-    from tornado.options import parse_command_line
-    parse_command_line()
-    if options.settings:
-        parse_config_file(options.settings)
-
+def create_application():
     settings = dict(
         debug=options.debug,
         autoescape=options.autoescape,
@@ -116,7 +106,19 @@ def main():
     application.register_filter('timesince', timesince)
     application.register_filter('topiclink', topiclink)
 
+    return application
+
+
+def main():
+    from july.util import parse_config_file
+    from tornado.options import parse_command_line
+    parse_command_line()
+    if options.settings:
+        parse_config_file(options.settings)
+
+    application = create_application()
     run_server(application)
+
 
 if __name__ == "__main__":
     main()
