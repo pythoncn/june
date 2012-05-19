7cad79ed181f4d97b5ef273eef3797a96eb285b2
lepture <lepture@me.com>
2012-05-19 17:57:22 +0800

    remove mail

--- june/mail/__init__.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-__appname__ = 'June Mail'
-__version__ = "0.1 alpha"
--- june/mail/tasks.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-
-# TODO add celery tasks
-# TODO extract independent mail sending class from MailHandler,
-# so that it could be used here
