1d27c15f613415e0d03e984c54326133f3045949
lepture <lepture@me.com>
2012-05-31 12:31:16 +0800

    remove useless img, settings example

--- settings.py
+++ settings.py
@@ -1,28 +1,45 @@
-# Pre-defined settings.py, play with it as you wish.
+#debug = False
+#address = '0.0.0.0'
+sqlalchemy_engine = "mysql://user@localhost:3306/db?charset=utf8"
+memcache = "127.0.0.1:11211"
+cookie_secret = "secret"
+password_secret = "secret"
+static_url_prefix = '/static/'
 
-#: site configuration
 sitename = "June"
-siteurl = ''
-cookie_secret = 'secret'
-password_secret = 'password_secret'
+siteurl = "http://127.0.0.1:8000"
+#: if you have another sitefeed
+#sitefeed = ""
 
-#: Default listening address (defined in July) is 127.0.0.1
-# address = '0.0.0.0'
+#: google analytics
+#ga = ""
 
-
-#: sqlalchemy database
-sqlalchemy_engine = "sqlite:///june.sqlite"
-sqlalchemy_kwargs = {"echo": True}
-
-
-#: third party support
+#: register your recaptcha at www.google.com/recaptcha
+#: this is a public key
 recaptcha_key = '6Le9jtESAAAAAGBuFoL451V-0Vt-_xiNA8ACQiTd'
 recaptcha_secret = '6Le9jtESAAAAAO2f24yTcAEnrnzUv35B1ofONIlR'
 
-google_custom_search = "017842580319746762888:veoakwnpkqq"
+#: if you have a gravatar proxy
+#gravatar_base_url = ''
+#gravatar_extra = ''
 
 emoji_url = 'http://python-china.b0.upaiyun.com/emojis/'
+
+#twitter_key = ''
+#twitter_secret = ''
 
 image_backend = 'june.front.backends.LocalBackend'
 local_static_path = '/tmp/'
 local_static_url = 'http://path.to.com/'
+
+#: upyun as image backend
+#image_backend = 'june.front.backends.UpyunBackend'
+#upyun_username = 'username'
+#upyun_password = 'passowrd'
+#upyun_bucket = 'bucket/directory'
+
+#: smtp
+smtp_user = 'noreply@gmail.com'
+smtp_password = 'password'
+smtp_host = 'smtp.gmail.com'
+smtp_ssl = True
