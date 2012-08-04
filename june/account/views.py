3f40f8df115bfe1d5facaf4894b5f2f8e13efcde
youngsterxyf <sas.198708@gmail.com>
2012-08-04 18:32:00 +0800

    添加账户设置功能account/setting

--- june/account/views.py
+++ june/account/views.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import render_template
 from flask import request
 from .models import db, Member
-from .helpers import login, logout
+from .helpers import login, logout, get_current_user
 
 app = Blueprint('account', __name__)
 
@@ -40,3 +40,25 @@ def signup():
         db.session.add(user)
         db.session.commit()
     return render_template('account/signup.html')
+
+@app.route('/setting', methods=['GET', 'POST'])
+def setting():
+    if request.method == 'POST':
+        username = request.form['username']
+        website = request.form['website']
+        city = request.form['city']
+        description = request.form['description']
+        user = get_current_user()
+        if user:
+            user.username = username
+            user.website = website
+            user.city = city
+            user.description = description
+            db.session.commit()
+    user = get_current_user()
+    if user: 
+        username = user.username
+        website = user.website
+        city = user.city
+        description = user.description
+    return render_template('account/setting.html', username = username, website = website, city = city, description = description)
