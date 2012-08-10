ed74c35b7a6c4559187fae878c325318fc02c6b1
lepture <lepture@me.com>
2012-08-11 00:11:11 +0800

    create node app tables

--- syncdb.py
+++ syncdb.py
@@ -3,5 +3,6 @@ from june.app import app, prepare_app
 app.config.from_pyfile('_config/development.py')
 prepare_app()
 
-from june.account import models
-models.db.create_all()
+from june import account, node
+account.models.db.create_all()
+node.models.db.create_all()
