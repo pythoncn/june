90e1cf0168aa7f6eb5e1f5f8fadc583f3a757656
lepture <lepture@me.com>
2012-09-01 15:57:03 +0800

    topic view

--- june/topic/views.py
+++ june/topic/views.py
@@ -2,24 +2,20 @@
 
 from flask import Blueprint
 from flask import render_template
-from flask import request, redirect, url_for
-from flask import flash
+from flask import request
 from flask import g
-from .models import Topic, Reply, Vote, TopicLog
+from june.node.models import Node
+from .models import Topic
 
 app = Blueprint('topic', __name__, template_folder='templates')
 
 
-@app.route('/-create', methods=['GET'])
-def create():
-    nodes = Node.query.all()
-    return render_template("create_topic.html", nodes=nodes)
+@app.route('/-create/<slug>')
+def create(slug):
+    node = Node.query.filter_by(slug=slug).first_or_404()
+    return render_template("create_topic.html")
 
 
 @app.route('/<int:id>/-edit', methods=['GET', 'POST'])
 def edit(id):
-    if request.method == 'POST':
-        topic = Topic.query.get_or_404(id)
-
-    if request.method == 'GET':
-        pass
+    pass
