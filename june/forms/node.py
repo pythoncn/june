f9fe021f1c804fc5cc9944dd0c83254f4dca23fe
Hsiaoming Yang <me@lepture.com>
2013-05-06 22:34:54 +0800

    add validation on urlname of node

--- june/forms/node.py
+++ june/forms/node.py
@@ -19,6 +19,12 @@ class NodeForm(BaseForm):
     description = TextAreaField(_('Description'))
     role = IntegerField(description=_('Required role'), default=1)
 
+    def validate_urlname(self, field):
+        if self._obj and self._obj.urlname == field.data:
+            return
+        if Node.query.filter_by(urlname=field.data).count():
+            raise ValueError(_('The node exists'))
+
     def save(self):
         node = Node(**self.data)
         node.save()
