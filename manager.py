ea8aa90491e97e8f173b061ee7c5a4b1e62ddaf4
Hsiaoming Yang <me@lepture.com>
2013-05-13 11:02:59 +0800

    we can profile the program now

--- manager.py
+++ manager.py
@@ -14,18 +14,26 @@ manager = Manager(app)
 
 
 @manager.command
-def runserver(port=5000):
+def runserver(port=5000, with_profile=False):
     """Runs a development server."""
     from gevent.wsgi import WSGIServer
     from werkzeug.serving import run_with_reloader
     from werkzeug.debug import DebuggedApplication
+    from werkzeug.contrib.profiler import ProfilerMiddleware
 
     port = int(port)
+
+    if with_profile:
+        f = open('./data/profile.log', 'w')
+        wsgi = ProfilerMiddleware(app, f)
+    else:
+        wsgi = DebuggedApplication(app)
 
     @run_with_reloader
     def run_server():
         print('start server at: 127.0.0.1:%s' % port)
-        http_server = WSGIServer(('', port), DebuggedApplication(app))
+
+        http_server = WSGIServer(('', port), wsgi)
         http_server.serve_forever()
 
     run_server()
