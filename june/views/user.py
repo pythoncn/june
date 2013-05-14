b06f7efb282ff37d81a2904b26186e05251c33cf
Hsiaoming Yang <me@lepture.com>
2013-05-14 22:30:23 +0800

    user profile with topics

--- june/views/user.py
+++ june/views/user.py
@@ -3,7 +3,7 @@
 from flask import Blueprint, request
 from flask import render_template, abort
 from ..helpers import force_int
-from ..models import Account
+from ..models import Account, Topic, fill_with_nodes
 
 
 __all__ = ['bp']
@@ -35,5 +35,10 @@ def view(username):
 
     :param username: the username of a user.
     """
+    page = force_int(request.args.get('page', 1), 0)
+    if not page:
+        return abort(404)
     user = Account.query.filter_by(username=username).first_or_404()
-    return render_template('user/view.html', user=user)
+    topics = Topic.query.order_by(Topic.id.desc()).limit(16)
+    topics = fill_with_nodes(topics)
+    return render_template('user/view.html', user=user, topics=topics)
