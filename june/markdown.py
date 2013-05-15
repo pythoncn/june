3ca7992d5efcd790184a9c463f068a93fda4d6b3
Hsiaoming Yang <me@lepture.com>
2013-05-15 22:20:57 +0800

    fix on embed links

--- june/markdown.py
+++ june/markdown.py
@@ -9,40 +9,56 @@ class JuneRenderer(m.HtmlRenderer, m.SmartyPants):
         title = link.replace('http://', '').replace('https://', '')
 
         #: youtube.com
-        pattern = r'http://www.youtube.com/watch\?v=([a-zA-Z0-9\-\_]+)'
+        pattern = r'http://www\.youtube\.com\/watch\?v=([a-zA-Z0-9\-\_]+)'
         match = re.match(pattern, link)
         if not match:
-            pattern = r'http://youtu.be/([a-zA-Z0-9\-\_]+)'
+            pattern = r'http:\/\/youtu.be\/([a-zA-Z0-9\-\_]+)'
             match = re.match(pattern, link)
         if match:
-            value = ('<iframe width="560" height="315" src='
-                     '"http://www.youtube.com/embed/%(id)s" '
-                     'frameborder="0" allowfullscreen></iframe>'
-                     '<div><a rel="nofollow" href="%(link)s">'
-                     '%(title)s</a></div>'
-                    ) % {'id': match.group(1), 'link': link, 'title': title}
+            value = (
+                '<iframe width="560" height="315" src='
+                '"http://www.youtube.com/embed/%(id)s" '
+                'frameborder="0" allowfullscreen></iframe>'
+                '<div><a rel="nofollow" href="%(link)s">'
+                '%(title)s</a></div>'
+            ) % {'id': match.group(1), 'link': link, 'title': title}
             return value
 
         #: gist support
-        pattern = r'(https?://gist.github.com/[\d]+)'
+        pattern = r'(https?:\/\/gist\.github\.com\/.+\d+)'
         match = re.match(pattern, link)
         if match:
-            value = ('<script src="%(link)s.js"></script>'
-                     '<div><a rel="nofollow" href="%(link)s">'
-                     '%(title)s</a></div>'
-                    ) % {'link': match.group(1), 'title': title}
+            value = (
+                '<script src="%(link)s.js"></script>'
+                '<div><a rel="nofollow" href="%(link)s">'
+                '%(title)s</a></div>'
+            ) % {'link': match.group(1), 'title': title}
             return value
 
         #: vimeo.com
-        pattern = r'http://vimeo.com/([\d]+)'
+        pattern = r'https?:\/\/vimeo\.com\/([\d]+)'
         match = re.match(pattern, link)
         if match:
-            value = ('<iframe width="500" height="281" frameborder="0" '
-                     'src="http://player.vimeo.com/video/%(id)s" '
-                     'allowFullScreen></iframe>'
-                     '<div><a rel="nofollow" href="%(link)s">'
-                     '%(title)s</a></div>'
-                    ) % {'id': match.group(1), 'link': link, 'title': title}
+            value = (
+                '<iframe width="566" height="318" frameborder="0" '
+                'src="https://player.vimeo.com/video/%(id)s" '
+                'allowFullScreen></iframe>'
+                '<div><a rel="nofollow" href="%(link)s">'
+                '%(title)s</a></div>'
+            ) % {'id': match.group(1), 'link': link, 'title': title}
+            return value
+
+        #: ascii.io
+        pattern = r'(http:\/\/ascii\.io\/a\/\d+)'
+        match = re.match(pattern, link)
+        if match:
+            value = (
+                '<iframe width="566" height="600" frameborder="0" '
+                'src="%(url)s/raw" '
+                'allowFullScreen></iframe>'
+                '<div><a rel="nofollow" href="%(link)s">'
+                '%(title)s</a></div>'
+            ) % {'url': match.group(1), 'link': link, 'title': title}
             return value
 
         if is_email:
