8e708e4d58ebcd0029731d5bd8456481dab55737
Hsiaoming Yang <me@lepture.com>
2013-05-16 17:15:59 +0800

    prepare for upload handler

--- june/views/front.py
+++ june/views/front.py
@@ -2,6 +2,7 @@
 
 from flask import Blueprint
 from flask import render_template
+from ..helpers import require_user
 from ..models import Node, Topic, fill_topics
 
 
@@ -10,8 +11,16 @@ bp = Blueprint('front', __name__)
 
 @bp.route('/')
 def home():
+    """The homepage of the site."""
     topics = Topic.query.order_by(Topic.id.desc()).limit(16)
     topics = fill_topics(topics)
-
     nodes = Node.query.order_by(Node.id.desc()).limit(10)
     return render_template('index.html', topics=topics, nodes=nodes)
+
+
+@bp.route('/upload', methods=['POST'])
+@require_user
+def upload():
+    """Upload images handler."""
+    #TODO
+    return ''
