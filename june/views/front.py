463f553610c5040265abb7f32eed6a0260f976f0
Hsiaoming Yang <me@lepture.com>
2013-05-16 18:49:45 +0800

    show blogs at homepage

--- june/views/front.py
+++ june/views/front.py
@@ -15,7 +15,18 @@ def home():
     topics = Topic.query.order_by(Topic.id.desc()).limit(16)
     topics = fill_topics(topics)
     nodes = Node.query.order_by(Node.id.desc()).limit(10)
-    return render_template('index.html', topics=topics, nodes=nodes)
+
+    # blog is a special node, get the latest posts from blog
+    blog = Node.query.filter_by(urlname='blog').first()
+    if blog:
+        blogs = Topic.query.filter_by(
+            node_id=blog.id).order_by(Topic.id.desc()).limit(2)
+    else:
+        blogs = None
+
+    return render_template(
+        'index.html', topics=topics, nodes=nodes, blogs=blogs
+    )
 
 
 @bp.route('/upload', methods=['POST'])
