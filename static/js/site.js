07967d9cf6488e5c55c97ff37c01adbe684e7399
Hsiaoming Yang <me@lepture.com>
2013-05-23 10:31:45 +0800

    preview finished. #39

--- static/js/site.js
+++ static/js/site.js
@@ -1,5 +1,4 @@
 (function() {
-
   // delete a reply
   $('.reply-list .delete').click(function() {
     var self = $(this);
@@ -14,4 +13,30 @@
     return false;
   });
 
+  // preview markdown
+  var preview = document.createElement('div');
+  $('.preview-button').click(function(e) {
+    e.preventDefault();
+    var self = $(this);
+    var target = $(self.data('target'));
+    if (self.hasClass('is-previewing')) {
+      // toggle off
+      preview.remove();
+      target.show();
+      self.removeClass('is-previewing');
+      return false;
+    }
+    self.addClass('is-previewing');
+    var content = target.val();
+    $.post('/markdown', {content: content}, function(data) {
+      preview.innerHTML = data;
+      target.hide();
+      target.after(preview);
+    });
+    $(preview).on('click', function() {
+      preview.remove()
+      target.show();
+    });
+    return false;
+  });
 })();
