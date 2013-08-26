602228a192d8a891172d6a8788cd41b92035ffc7
Hsiaoming Yang <me@lepture.com>
2013-08-26 10:31:13 +0800

    Add nginx config, add fabric, changes in configuration

--- etc/gunicorn.py
+++ etc/gunicorn.py
@@ -2,25 +2,22 @@
 
 import gevent.monkey
 gevent.monkey.patch_all()
+
 import multiprocessing
 
-#bind = 'unix:/path/to/june.sock'
-bind = '127.0.0.1:8000'
+bind = 'unix:/home/{{user}}/var/run/june.sock'
+
 workers = multiprocessing.cpu_count() * 2 + 1
 worker_class = 'egg:gunicorn#gevent'
 
 # you should change this
-user = 'lepture'
+user = '{{user}}'
 
 # maybe you like error
-accesslog = '-'
 loglevel = 'warning'
 errorlog = '-'
 
 secure_scheme_headers = {
     'X-SCHEME': 'https',
-    'X-FORWARDED-PROTOCOL': 'ssl',
-    'X-FORWARDED-PROTO': 'https',
-    'X-FORWARDED-SSL': 'on',
 }
 x_forwarded_for_header = 'X-FORWARDED-FOR'
