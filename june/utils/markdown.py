e51b032ebf20efc216e8e185b6febd5bca1bed2f
Hsiaoming Yang <me@lepture.com>
2013-12-02 16:57:47 +0800

    New renderer for markdown.

--- june/utils/markdown.py
+++ june/utils/markdown.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 """
-    june.markdown
-    ~~~~~~~~~~~~~
+    june.utils.markdown
+    ~~~~~~~~~~~~~~~~~~~
 
     Markdown parser for June.
 
@@ -11,6 +11,7 @@
 import re
 import misaka as m
 from markupsafe import escape
+from jinja2 import Markup
 from pygments import highlight
 from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
@@ -19,7 +20,9 @@ from pygments.formatters import HtmlFormatter
 class AutolinkRenderer(m.HtmlRenderer):
     def autolink(self, link, is_email):
         if is_email:
-            return '<a href="mailto:%(link)s">%(link)s</a>' % {'link': link}
+            return Markup(
+                '<a href="mailto:%(link)s">%(link)s</a>' % {'link': link}
+            )
         title = link.replace('http://', '').replace('https://', '')
 
         #: youtube.com
@@ -36,7 +39,7 @@ class AutolinkRenderer(m.HtmlRenderer):
                 '<div><a rel="nofollow" href="%(link)s">'
                 '%(title)s</a></div>'
             ) % {'id': match.group(1), 'link': link, 'title': title}
-            return value
+            return Markup(value)
 
         #: gist support
         pattern = r'(https?:\/\/gist\.github\.com\/.+\d+)'
@@ -47,7 +50,7 @@ class AutolinkRenderer(m.HtmlRenderer):
                 '<div><a rel="nofollow" href="%(link)s">'
                 '%(title)s</a></div>'
             ) % {'link': match.group(1), 'title': title}
-            return value
+            return Markup(value)
 
         #: vimeo.com
         pattern = r'https?:\/\/vimeo\.com\/([\d]+)'
@@ -60,7 +63,7 @@ class AutolinkRenderer(m.HtmlRenderer):
                 '<div><a rel="nofollow" href="%(link)s">'
                 '%(title)s</a></div>'
             ) % {'id': match.group(1), 'link': link, 'title': title}
-            return value
+            return Markup(value)
 
         #: ascii.io
         pattern = r'(http:\/\/ascii\.io\/a\/\d+)'
@@ -73,13 +76,20 @@ class AutolinkRenderer(m.HtmlRenderer):
                 '<div><a rel="nofollow" href="%(link)s">'
                 '%(title)s</a></div>'
             ) % {'url': match.group(1), 'link': link, 'title': title}
-            return value
+            return Markup(value)
+
+    def paragraph(self, text):
+        pattern = re.compile(r'\s@(\w+)')
+        text = pattern.sub(r' @<a href="/user/\1">\1</a>', text)
+        pattern = re.compile(r'^@(\w+)')
+        text = pattern.sub(r'@<a href="/user/\1">\1</a>', text)
+        return Markup('<p>%s</p>' % text)
 
 
 class HighlightRenderer(AutolinkRenderer):
     def block_code(self, text, lang):
         if not lang:
-            return u'<pre><code>%s</code></pre>' % escape(text)
+            return Markup('<pre><code>%s</code></pre>' % escape(text))
 
         inlinestyles = False
         linenos = False
@@ -93,33 +103,40 @@ class HighlightRenderer(AutolinkRenderer):
             formatter = HtmlFormatter(
                 noclasses=inlinestyles, linenos=linenos
             )
-            return highlight(text, lexer, formatter)
+            return Markup(highlight(text, lexer, formatter))
         except:
-            return '<pre class="%s"><code>%s</code></pre>' % (
+            return Markup('<pre class="%s"><code>%s</code></pre>' % (
                 lang, escape(text)
-            )
+            ))
 
 
-def markdown(text, highlight=True, inlinestyles=False, linenos=False):
+class PlainRenderer(AutolinkRenderer):
+    def header(self, text, level):
+        return Markup('<div class="md-h%d">%s</div>' % (level, text))
+
+
+def markdown(text, renderer='highlight', inlinestyles=False, linenos=False):
     """Markdown filter for writeup.
 
     :param text: the content to be markdownify
-    :param highlight: highlight the code block or not
+    :param renderer: choose a renderer, default is HighlightRenderer
     :param inlinestyles: highlight the code with inline styles
     :param linenos: show linenos of the highlighted code
     """
     if not text:
         return u''
-    if highlight:
-        renderer = HighlightRenderer()
-        renderer._inlinestyles = inlinestyles
-        renderer._linenos = linenos
+    if renderer == 'highlight':
+        r = HighlightRenderer()
+        r._inlinestyles = inlinestyles
+        r._linenos = linenos
+    elif renderer == 'plain':
+        r = PlainRenderer()
     else:
-        renderer = AutolinkRenderer()
+        r = AutolinkRenderer()
 
     extensions = (
         m.EXT_NO_INTRA_EMPHASIS | m.EXT_FENCED_CODE | m.EXT_AUTOLINK |
         m.EXT_TABLES | m.EXT_STRIKETHROUGH | m.EXT_SUPERSCRIPT
     )
-    md = m.Markdown(renderer, extensions=extensions)
+    md = m.Markdown(r, extensions=extensions)
     return md.render(text)
