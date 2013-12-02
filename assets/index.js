0f29b23b2e0e681bd1ba297ec4f13c85a51b1dbf
Hsiaoming Yang <me@lepture.com>
2013-12-02 16:37:26 +0800

    JS for comment

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,29 @@
 require('bootstrap');
 
-exports.jQuery = require('jquery');
+var $ = require('jquery');
+exports.jQuery = $;
+
+$('.comment .delete').on('click', function() {
+  var item = $(this);
+  var id = item.attr('href').slice(1);
+  var url = location.pathname + '/reply?reply=' + id;
+  $.ajax({
+    url: url,
+    method: 'DELETE',
+    success: function() {
+      item.parentsUntil('.comment').parent().fadeOut();
+    }
+  });
+  return false;
+});
+
+$('.comment .reply').on('click', function() {
+  var item = $(this);
+  var username = item.attr('href').slice(1);
+  var form = $('.comment-form textarea');
+  var value = form.val();
+  value += '@' + username;
+  form.val(value);
+  form.focus();
+  return false;
+});
