36e6c402d877024839ea7a402f39c36d30901162
Hsiaoming Yang <me@lepture.com>
2014-12-23 18:15:17 +0800

    Update deploy scripts

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,10 @@
 # coding: utf-8
 
-from fabric.api import env, local, cd, run
+from fabric.api import env, local, cd, run, sudo
 from fabric.operations import put
 
-env.user = 'www'
-env.hosts = ['python-china.org']
-# env.password
-
-
-def prepare():
-    """Prepare server for installation."""
-    run('mkdir -p ~/venv')
-    run('virtualenv ~/venv/june')
-    run('mkdir -p ~/apps/june/public/static')
+env.use_ssh_config = True
+env.keepalive = 60
 
 
 def tarball():
@@ -24,6 +16,8 @@ def tarball():
 def upload():
     """Upload tarball to the server."""
     dist = local('python setup.py --fullname', capture=True).strip()
+    run('rm -f ~/tmp/june.tar.gz')
+    sudo('rm -fr ~/tmp/june')
     run('mkdir -p ~/tmp/june')
 
     put('dist/%s.tar.gz' % dist, '~/tmp/june.tar.gz')
@@ -35,33 +29,35 @@ def install():
     """Install june package."""
     dist = local('python setup.py --fullname', capture=True).strip()
     with cd('~/tmp/june/%s' % dist):
-        run('~/venv/june/bin/python setup.py install')
-
-
-def clean():
-    """Clean packages on server."""
-    run('rm -fr ~/tmp/june')
-    run('rm -f ~/tmp/june.tar.gz')
-
-
-def update():
-    """Update assets"""
-    dist = local('python setup.py --fullname', capture=True).strip()
-    tmpdir = '~/tmp/june/%s' % dist
-    run('cp -r %s/june/public ~/apps/june/' % tmpdir)
-
-
-def upgrade():
-    """Upgrade database"""
-    dist = local('python setup.py --fullname', capture=True).strip()
-    tmpdir = '~/tmp/june/%s' % dist
-    run('cp %s/alembic.ini ~/apps/june/' % tmpdir)
-    run('rm -fr ~/apps/june/alembic')
-    run('cp -r %s/alembic ~/apps/june/' % tmpdir)
-    with cd('~/apps/june'):
-        run('~/venv/june/bin/alembic upgrade head')
+        sudo('/srv/venv/june/bin/pip install .')
 
 
 def restart():
     """Restart remote server"""
     run('supervisorctl pid june | xargs kill -HUP')
+
+
+def bootstrap():
+    dist = local('python setup.py --fullname', capture=True).strip()
+    tarball()
+    upload()
+
+    # prepare
+    sudo('mkdir -p /var/log/june')
+    sudo('mkdir -p /srv/venv')
+    sudo('virtualenv /srv/venv/june')
+    sudo('mkdir -p /www/june/public/static')
+
+    install()
+    # install patch
+    sudo('/srv/venv/june/bin/pip install MySQL-python redis')
+
+    with cd('~/tmp/june/%s' % dist):
+        sudo('mv etc/nginx.conf /etc/nginx/conf.d/june.conf')
+        sudo('mv etc/supervisord.conf /etc/supervisor/conf.d/june.conf')
+
+        sudo('rm -fr /www/june/etc')
+        sudo('mv etc /www/june/')
+        sudo('rm -fr /www/june/public')
+        sudo('mv june/public /www/june/')
+        sudo('mv wsgi.py /www/june/')
